a977dfa4b694e175868a185884add2fdc8604117
felibuscaglia <felipebenjamin1810@gmail.com>
2022-02-08 09:53:01 -0300

    Adds styles to action buttons

--- src/components/Toolbar/ActionButtons.tsx
+++ src/components/Toolbar/ActionButtons.tsx
@@ -1,7 +1,42 @@
+import { HiOutlinePencil, HiOutlineMinus } from "react-icons/hi";
+import { RiText, RiEraserFill } from "react-icons/ri";
+import { BiUndo } from "react-icons/bi";
+import style from "./styles.module.scss";
+import { buttonBlackColor } from "../../shared/constants";
+import { useState } from "react";
+
+const icons = [
+  { icon: HiOutlinePencil },
+  { icon: HiOutlineMinus },
+  { icon: RiText },
+  { icon: RiEraserFill },
+  { icon: BiUndo },
+];
+
+const DEFAULT_SELECTED_OPTION = 0;
+
 const ActionButtons = () => {
+  const [selectedOption, setSelectedOption] = useState(DEFAULT_SELECTED_OPTION);
+  const [hoveredBtn, setHoveredBtn] = useState<null | number>(null);
+
   return (
-    <div>Action buttons</div>
-  )
-}
+    <div id={style.actionButtons}>
+      {icons.map(({ icon: Icon }, i) => (
+        <Icon
+          size={"1.875rem"}
+          color={
+            selectedOption === i || hoveredBtn === i
+              ? buttonBlackColor
+              : "#C8C8C8"
+          }
+          cursor="pointer"
+          onClick={() => setSelectedOption(i)}
+          onMouseEnter={() => setHoveredBtn(i)}
+          onMouseLeave={() => setHoveredBtn(null)}
+        />
+      ))}
+    </div>
+  );
+};
 
 export default ActionButtons;
